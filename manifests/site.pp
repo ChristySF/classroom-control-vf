4a1b8b31ea6fe543fe3ef4f7e3c33eb80a97fe3d
ChristySF <christy.ward.c1ao@statefarm.com>
2016-05-24 10:10:06 -0500

    Update site.pp
    corrected name of cowsay

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 
-  exec { "cosway 'Welcome to $(::fqdn)!' > /etc/motd":
+  exec { "cowsay 'Welcome to $(::fqdn)!' > /etc/motd":
     path    => '/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
   }
