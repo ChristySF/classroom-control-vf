06da7f8fbf061e6c53c5c588b858b63de225826b
ChristySF <christy.ward.c1ao@statefarm.com>
2016-05-25 09:34:39 -0500

    Update init.pp
    updating content for owner, group, perms.

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,10 +2,16 @@ class skeleton {
 
   file { '/etc/skel':
     ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
   
   file { '/etc/skel/.bashrc':
     ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
     source => ,
   }
 
