a89475faa38fd62759955c188081abe5562c4146
ChristySF <christy.ward.c1ao@statefarm.com>
2016-05-26 10:01:43 -0500

    Update init.pp
    replaced values with template.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -63,36 +63,62 @@ class nginx {
      require  => File['nginx rpm'], 
    } 
    
+   case $::osfamily {
+     'redhat','debian' : {
+       $package = 'nginx'
+       $owner   = 'root'
+       $group   = 'root'
+       $docroot = '/var/www/'
+       $confdir = '/etc/nginx'
+       $logdir  = '/var/log/nginx'
+     }
+     'windows' : {
+       $package = 'nginx-service'
+       $owner   = 'Administrator'
+       $group   = 'Administrator'
+       $docroot = 'C:/ProgramData/nginx/html'
+       $confdir = 'C:/ProgramData/nginx'
+       $logdir  = 'C:/ProgramData/nginx/logs'
+      }
+      default : {
+        fail("Module $(modules_name) is not supported on ${::osfamily}")
+      }
+   }
+   
+   $user = $::osfamily ? {
+     'redhat'  => 'nginx',
+     'debian'  => 'www-data',
+     'windows' => 'nobody',
+   }
+   
    File {
      owner   => 'root',
      group   => 'root',
      mode    => '0644',
    }
    
-   file { '/var/www/' :  
+   package { $package: 
+     ensure => present,
+   }
+     
+   file { [$docroot, "$(confdir)/conf.d"] :  
      ensure  => directory,  
-     require => Package['nginx'],
    }  
    
-   file { '/var/www/index.html' :  
+   file { "$(docroot)/index.html" :  
      ensure  => file,  
      source  => 'puppet:///modules/nginx/index.html',  
-     require => Package['nginx'],
    }  
    
-   file { 'nginx conf' :  
+   file { "$(confdir)/nginx conf" :  
      ensure  => file,  
-     path    => '/etc/nginx/nginx.conf',  
-     source  => 'puppet:///modules/nginx/nginx.conf',  
-     require => Package['nginx'],
+     content => template('nginx/nginx.conf.erb'),
      notify  => Service['nginx'],  
    }  
    
-   file { 'default conf' :  
+   file { "$(confdir)/conf.d/default conf" :  
      ensure  => file,  
-     path    => '/etc/nginx/conf.d/default.conf',  
-     source  => 'puppet:///modules/nginx/default.conf',  
-     require => Package['nginx'],
+     content => template('nginx/default.conf.erb'),
      notify  => Service['nginx'],  
    }  
    
